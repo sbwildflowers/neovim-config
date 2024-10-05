579cfd9c3bfdea8975a0cd78bc30501bc1d8b668
dmcleod2 <dmcleod@craftyapes.com>
2024-10-05 09:06:42 -0700

    adding some extra file searching into neovim

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,1 +1,10 @@
-require('lualine').setup()
+require('lualine').setup({
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        path = 1  -- 0 = just filename, 1 = relative path, 2 = absolute path
+      }
+    },  -- This shows the full file path
+  },
+})
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,4 +4,16 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep < ") });
 end)
+vim.keymap.set('n', '<leader>ff', function()
+	local searchterm = vim.fn.input("Grep < ")
+	if searchterm == '' then
+		searchterm = vim.fn.expand('cword')
+	end
+
+	local filetype = vim.fn.input('File type < ')
+	if filetype == '' then
+		filetype = '*.*'
+	end
+	builtin.live_grep({ default_text = searchterm, glob_pattern = filetype })
+end)
 require("telescope").load_extension("git_worktree")
